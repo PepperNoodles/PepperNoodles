1504e0261d8da2271677b324065f14715c33204a
steve <stevenzchao439@gmail.com>
2021-04-22 16:17:07 +0800

    commit again about user comments

--- src/main/java/com/infotran/springboot/loginsystem/controller/UserAccountController.java
+++ src/main/java/com/infotran/springboot/loginsystem/controller/UserAccountController.java
@@ -126,9 +126,8 @@ public class UserAccountController {
 		System.out.println("fuck======================================================0");
 //		UserAccount useraccount1 = new UserAccount();
 		useraccount1.setAccountIndex(dispatch.get("accountIndex"));
-		useraccount1.setPassword(dispatch.get("password"));
 		//幫密碼加密
-		String bcEncode1 = new BCryptPasswordEncoder().encode(useraccount1.getPassword());
+		String bcEncode1 = new BCryptPasswordEncoder().encode(dispatch.get("password"));
 		useraccount1.setPassword(bcEncode1);
 		userDetail.setRealName(dispatch.get("realName"));
 		userDetail.setNickName(dispatch.get("nickname"));
