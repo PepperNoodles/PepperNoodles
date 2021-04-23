fe99273a593b3cd6240c7c37b8add5869eb3d8dc
hedy <ting0420a@gmail.com>
2021-04-23 16:11:35 +0800

    企業新增Menu、使用者瀏覽Menu完成

--- src/main/java/com/infotran/springboot/SpringPepperNoodleApplication.java
+++ src/main/java/com/infotran/springboot/SpringPepperNoodleApplication.java
@@ -1,7 +1,13 @@
 package com.infotran.springboot;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 
 @SpringBootApplication
 public class SpringPepperNoodleApplication {
@@ -10,12 +16,12 @@ public class SpringPepperNoodleApplication {
 		SpringApplication.run(SpringPepperNoodleApplication.class, args);
 	}
 
-//	@Bean 
-//	FilterRegistrationBean<Filter>  hiddenHttpMethodFilter(){
-//		FilterRegistrationBean<Filter>  filterBean = new FilterRegistrationBean<>();
-//		filterBean.setFilter(new HiddenHttpMethodFilter());
-//		return filterBean;
-//	}
+	@Bean 
+	FilterRegistrationBean<Filter>  hiddenHttpMethodFilter(){
+		FilterRegistrationBean<Filter>  filterBean = new FilterRegistrationBean<>();
+		filterBean.setFilter(new HiddenHttpMethodFilter());
+		return filterBean;
+	}
 	
 	
 	
