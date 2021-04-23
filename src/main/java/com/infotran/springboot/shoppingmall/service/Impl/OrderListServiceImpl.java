d8dd2d73cf694bdd1217b3e5f38dbe173b626eb7
Chris Luo <chrislo5311@gmail.com>
2021-04-23 16:21:31 +0800

    update user orderlist and messagebox

--- src/main/java/com/infotran/springboot/shoppingmall/service/Impl/OrderListServiceImpl.java
+++ src/main/java/com/infotran/springboot/shoppingmall/service/Impl/OrderListServiceImpl.java
@@ -1,10 +1,11 @@
 package com.infotran.springboot.shoppingmall.service.Impl;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.infotran.springboot.shoppingmall.dao.OrderListRepository;
 import com.infotran.springboot.shoppingmall.model.OrderList;
+import com.infotran.springboot.shoppingmall.model.OrderListView;
 import com.infotran.springboot.shoppingmall.service.OrderListService;
 
 @Service
@@ -17,5 +18,13 @@ public class OrderListServiceImpl implements OrderListService {
 	public OrderList save(OrderList olist) {
 		return ordao.save(olist);
 	}
+	
+	
+	@Override
+	public ArrayList<OrderListView> findOrderListView(String username) {
+		return ordao.findOrderList(username);
+	}
+	
+	
 
 }
